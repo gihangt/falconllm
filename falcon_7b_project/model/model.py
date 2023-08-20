0db54403360840a19a97aba4dab292a300cdebc3
dandenig <Gihan.DandeniyaHewaralalage@lincolnuni.ac.nz>
2023-08-21 09:14:21 +1200

    Add 4bit quantization

--- falcon_7b_project/model/model.py
+++ falcon_7b_project/model/model.py
@@ -1,10 +1,12 @@
 import torch
-from transformers import AutoTokenizer, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
 from typing import Dict
 
-CHECKPOINT = "tiiuae/falcon-7b-instruct"
+MODEL_NAME = "tiiuae/falcon-7b-instruct"
 DEFAULT_MAX_LENGTH = 128
-DEFAULT_TOP_P = 0.95
+
+import os
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb=5'
 
 
 class Model:
@@ -12,20 +14,34 @@ class Model:
         self._data_dir = data_dir
         self._config = config
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print("THE DEVICE INFERENCE IS RUNNING ON IS: ", self.device)
         self.tokenizer = None
         self.pipeline = None
 
     def load(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        model_8bit = AutoModelForCausalLM.from_pretrained(
+            MODEL_NAME,
+            device_map="auto",
+            load_in_8bit=False,
+            quantization_config=quantization_config,
+            offload_folder="offload",
+            trust_remote_code=True)
+
         self.pipeline = pipeline(
             "text-generation",
-            model=CHECKPOINT,
+            model=model_8bit,
             tokenizer=self.tokenizer,
             torch_dtype=torch.bfloat16,
             trust_remote_code=True,
             device_map="auto",
         )
-
 
     def predict(self, request: Dict) -> Dict:
         with torch.no_grad():
@@ -34,6 +50,7 @@ class Model:
                 data = self.pipeline(
                     prompt,
                     eos_token_id=self.tokenizer.eos_token_id,
+                    max_length=DEFAULT_MAX_LENGTH,
                     **request
                 )[0]
                 return {"data": data}
